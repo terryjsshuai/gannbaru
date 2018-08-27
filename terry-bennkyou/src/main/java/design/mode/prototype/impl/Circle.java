e9544b8c11338db5227e18adee6c843e4f20265c
terry jiang <Terry.Jiang@lombardrisk.com>
2018-08-27 15:01:01 +0800

    design : prototype

---
+++ terry-bennkyou/src/main/java/design/mode/prototype/impl/Circle.java
@@ -0,0 +1,15 @@
+package design.mode.prototype.impl;
+
+import design.mode.prototype.Shape;
+
+public class Circle extends Shape {
+
+    public Circle() {
+        type = "Circle";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
