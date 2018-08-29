ccff6da848dac96e6dc57160aa4d15c13e4fe7cf
terry jiang <Terry.Jiang@lombardrisk.com>
2018-08-29 17:52:04 +0800

    design : composite entity

---
+++ terry-bennkyou/src/main/java/design/pattern/compositeentity/DependentObject2.java
@@ -0,0 +1,14 @@
+package design.pattern.compositeentity;
+
+public class DependentObject2 {
+
+    private String data;
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+    public String getData(){
+        return data;
+    }
+}
