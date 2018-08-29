a504f61cbb2f30e6ec467cbce3d8e7431f8c4092
terry jiang <Terry.Jiang@lombardrisk.com>
2018-08-29 11:27:06 +0800

    design : Null Object

---
+++ terry-bennkyou/src/main/java/design/mode/nullobject/AbstractCustomer.java
@@ -0,0 +1,9 @@
+package design.mode.nullobject;
+
+public abstract class AbstractCustomer {
+    protected String name;
+
+    public abstract boolean isNil();
+
+    public abstract String getName();
+}
---
+++ terry-bennkyou/src/main/java/design/mode/nullobject/CustomerFactory.java
@@ -0,0 +1,20 @@
+package design.mode.nullobject;
+
+import java.util.Arrays;
+
+public class CustomerFactory {
+
+    public static final String[] names = {"Rob", "Joe", "Julie"};
+
+    public static AbstractCustomer getCustomer(String name) {
+        if (Arrays.stream(names).filter(s -> s.equalsIgnoreCase(name)).findFirst().isPresent()) {
+            return new RealCustomer(name);
+        }
+        /*for (int i = 0; i < names.length; i++) {
+            if (names[i].equalsIgnoreCase(name)) {
+                return new RealCustomer(name);
+            }
+        }*/
+        return new NullCustomer();
+    }
+}
