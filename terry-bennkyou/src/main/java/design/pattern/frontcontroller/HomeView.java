fb59d968f1e894efa6665d4f23ddec082cf3a8cf
terry jiang <Terry.Jiang@lombardrisk.com>
2018-08-30 10:51:06 +0800

    design : Front Controller

---
+++ terry-bennkyou/src/main/java/design/pattern/frontcontroller/HomeView.java
@@ -0,0 +1,7 @@
+package design.pattern.frontcontroller;
+
+public class HomeView {
+    public void show(){
+        System.out.println("Displaying Home Page");
+    }
+}
---
+++ terry-bennkyou/src/main/java/design/pattern/frontcontroller/StudentView.java
@@ -0,0 +1,7 @@
+package design.pattern.frontcontroller;
+
+public class StudentView {
+    public void show(){
+        System.out.println("Displaying Student Page");
+    }
+}
