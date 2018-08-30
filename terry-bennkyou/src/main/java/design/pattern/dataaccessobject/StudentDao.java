9fba074a5eac4cc00d413e8d1cde4aeea4cd1331
terry jiang <Terry.Jiang@lombardrisk.com>
2018-08-30 10:46:53 +0800

    design : Data Access Object

---
+++ terry-bennkyou/src/main/java/design/pattern/dataaccessobject/StudentDao.java
@@ -0,0 +1,10 @@
+package design.pattern.dataaccessobject;
+
+import java.util.List;
+
+public interface StudentDao {
+    List<Student> getAllStudents();
+    Student getStudent(int rollNo);
+    void updateStudent(Student student);
+    void deleteStudent(Student student);
+}
