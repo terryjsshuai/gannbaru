b804966904a7e065b48aba3a9551d041350172f8
terry jiang <Terry.Jiang@lombardrisk.com>
2018-08-31 16:11:53 +0800

    concurrent

---
+++ terry-bennkyou/src/main/java/concurrent/FutureUsage.java
@@ -0,0 +1,33 @@
+package concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureUsage {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        Callable<Object> task = () -> {
+
+            Thread.sleep(4000);
+
+            Object result = "finished";
+            return result;
+        };
+
+        Future<Object> future = executor.submit(task);
+        System.out.println("task submitted");
+
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+        } catch (ExecutionException e) {
+        }
+
+        // Thread won't be destroyed.
+    }
+}
---
+++ terry-bennkyou/src/main/java/concurrent/Volatile.java
@@ -0,0 +1,36 @@
+package concurrent;
+
+public class Volatile {
+
+    public static void main(String[] args) {
+        final Volatile volObj = new Volatile();
+        Thread t2 = new Thread(() -> {
+            while (true) {
+                volObj.check();
+            }
+        });
+        t2.start();
+        Thread t1 = new Thread(() -> {
+            while (true) {
+                volObj.swap();
+            }
+        });
+        t1.start();
+    }
+
+    boolean boolValue;// use volatile to print "WTF!"
+
+    public void check() {
+        if (boolValue == !boolValue)
+            System.out.println("WTF!");
+    }
+
+    public void swap() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        boolValue = !boolValue;
+    }
+}
